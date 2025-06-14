a1cd1125ed08e68c16ad1108d753d723b1083498
fernandoleiva94 <TuNuevoEmail@example.com>
2025-06-14 15:22:35 -0300

    update properties

--- src/main/java/com/sevenb/auth/auth_service/configuration/WebConfig.java
+++ src/main/java/com/sevenb/auth/auth_service/configuration/WebConfig.java
@@ -1,16 +1,20 @@
 package com.sevenb.auth.auth_service.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${auth.service.recipeFrontUtl}")
+    private String recipeFrontUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v1/auth/**") // Ajusta la ruta según sea necesario
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://d4bf-190-192-90-146.ngrok-free.app")// Ajusta esto si tu frontend está en otro lugar
+                .allowedOrigins(recipeFrontUrl)    // Ajusta esto si tu frontend está en otro lugar
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
